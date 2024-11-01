ecb88ba0399c6726c86a23b293e789737e5f6073
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-11-01 10:10:31 -0400

    Create Services.js

---
+++ src/components/Services.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Services = () => (
+  <div className="page-container">
+    <h1>Services</h1>
+    <p>Explore our services here.</p>
+  </div>
+);
+
+export default Services;
