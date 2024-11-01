350da58e4d9146c6ab2183b981752241a0fd2b7f
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-11-01 10:10:12 -0400

    Create Applicactions.js

---
+++ src/components/Applicactions.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Applications = () => (
+  <div className="page-container">
+    <h1>Applications</h1>
+    <p>Explore our application offerings here.</p>
+  </div>
+);
+
+export default Applications;
