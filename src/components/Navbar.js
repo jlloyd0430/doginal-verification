34c9230c461a4be90d6f168d35ee4fa14ab80a5f
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-11-03 06:21:10 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,13 +20,13 @@ const Navbar = ({ darkMode, toggleDarkMode }) => {
       <ul className={`nav-links ${menuOpen ? 'show' : ''}`}>
         <li><Link to="/applications" onClick={() => setMenuOpen(false)}>Apply</Link></li>
         <li><Link to="/services" onClick={() => setMenuOpen(false)}>Services</Link></li>
+        <li className="mobile-dark-mode-toggle">
+          <label className="switch">
+            <input type="checkbox" checked={darkMode} onChange={toggleDarkMode} />
+            <span className="slider"></span>
+          </label>
+        </li>
       </ul>
-      <div className="dark-mode-toggle">
-        <label className="switch">
-          <input type="checkbox" checked={darkMode} onChange={toggleDarkMode} />
-          <span className="slider"></span>
-        </label>
-      </div>
     </nav>
   );
 };
