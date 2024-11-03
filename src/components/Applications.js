da8941eb0675768065bd39fc8b16d196f5241e6a
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-11-03 06:08:04 -0500

    Update Applications.js

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Applications = () => (
   <div className="page-container">
     <h1>Applications</h1>
-    <p>Explore our application offerings here.</p>
+    <p>Would you like to add our Verification bot to your projects discord? we support drc-20 tokens, dunes, and nft projects alike! enquire from our services page.</p>
   </div>
 );
 
